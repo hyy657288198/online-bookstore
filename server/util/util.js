f6aa1343aeb7c36b9304aab15150094853906b3c
Yiyang Hua <55719746+hyy657288198@users.noreply.github.com>
2024-05-08 16:16:28 -0400

    Update util.js

---
+++ server/util/util.js
@@ -0,0 +1,12 @@
+// util.js
+
+const util = {
+    client_id: '748150668515-nrsc3n0enatn2p7ah7hbh1obl718v9nj.apps.googleusercontent.com',
+    client_secret: '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',
+    /*backend: 'http://localhost:4000',
+    frontend: 'http://localhost:3000',*/
+    backend: 'https://book-store-406822.ue.r.appspot.com',
+    frontend: 'https://frontend-dot-book-store-406822.ue.r.appspot.com',
+};
+
+module.exports = util;
